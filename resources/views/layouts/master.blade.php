b2bf8f3e65e00b02f0309b22c79dc689620e1201
JPGrefaldo <johnpaulgrefaldo@gmail.com>
2018-04-03 16:05:34 +0800

    Done with Counsel List

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -221,11 +221,8 @@
         @yield('content')
 
         <div class="footer">
-            <div class="pull-right">
-                10GB of <strong>250GB</strong> Free.
-            </div>
             <div>
-                <strong>Copyright</strong> Example Company &copy; 2014-2017
+                <strong>Copyright</strong> <a href="https://www.pacificblueit.com" target="_blank" >Pacific Blue I.T. &copy; {{ Date('Y') }}</a>
             </div>
         </div>
     </div>
